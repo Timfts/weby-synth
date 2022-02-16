e6b350a029c419c62d4cd118375293f335b6544a
Italo Fontes <italo5123@hotmail.com>
2022-02-16 00:39:58 -0300

    build: 📦 removed unused packages

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,7 @@
 import AppShell from "./components/AppShell";
 // @ts-ignore
-import init from "./assembly/build/optimized.wasm";
 import FirstSynth from "./components/synths/first-synth"
 /* import SecondSynth from "./components/synths/second-synth" */
-
-init().then((exports: any) => {
-  const { add, times } = exports;
-  /* console.log(add(2, 5));
-  console.log(times(2,3)) */
-});
 
 
 
